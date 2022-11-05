c1f241785e5e537b8565da58ea11e368883af59e
Charankasa07 <s180067@rguktsklm.ac.in>
2022-11-05 15:23:09 +0530

    updaate

--- index.js
+++ index.js
@@ -17,4 +17,4 @@ app.get('/',(req,res)=>{
     res.send("Hello World")
 })
 
-app.listen(3000,()=>{console.log("Listening on port 3000")})
+app.listen(process.env.port,()=>{console.log("Listening on port 3000")})
