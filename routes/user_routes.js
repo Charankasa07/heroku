c2ec6c19945e7e651564284a9af196bb09c29e82
Charankasa07 <s180067@rguktsklm.ac.in>
2022-11-09 23:31:41 +0530

    update

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -91,7 +91,7 @@ router.get('/reset-password/:email/:token',async (req,res)=>{
         const secret = process.env.token_customer + user.password
         const verified= jwt.verify(token,secret)
         if(verified){
-            res.sendFile(path.join(main_path+'/reset-password.html'))
+            res.send("password reset")
         }
 
     } catch (error) {
